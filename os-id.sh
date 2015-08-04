aace20b3d8005d8196f5ff95818724a3b4073044
Maxim Yegorushkin <maxim.yegorushkin@gmail.com>
2015-08-04 21:37:59 +0100

    Better OS version detection.

--- os-id.sh
+++ os-id.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ -r /etc/redhat-release ]]; then
-    awk -f - /etc/redhat-release <<'EOF'
-/CentOS release [0-9]+\.[0-9]+/ { print "centos-" $3 }
-/Fedora release [0-9]+/         { print "fedora-" $3 }
-/Red Hat Enterprise Linux Server release [0-9]+(\.[0-9]+)?/ { print "redhat-" $3 }
-EOF
+set -e
 
-fi
+[[ -f /etc/os-release ]] && . /etc/os-release && exec echo $ID-$VERSION_ID
+
+[[ -r /etc/redhat-release ]] && exec awk -f - /etc/redhat-release <<'EOF'
+/CentOS release [0-9]+\.[0-9]+/                             { print "centos-" $3 }
+/Fedora release [0-9]+/                                     { print "fedora-" $3 }
+/Red Hat Enterprise Linux Server release [0-9]+(\.[0-9]+)?/ { print "redhat-" $7 }
+EOF
